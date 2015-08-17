287ee5b2d67f53c6445060b70e5ad44251c073bc
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-08-17 04:11:04 +0000

    Initial commit

---
+++ Vagrantfile
@@ -0,0 +1,84 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+require 'fileutils'
+
+Vagrant.require_version ">= 1.6.0"
+
+# TODO get user runnning vagrant up
+# $host_home = File.expand_path "~"
+$box = "ubuntu/trusty64"
+$vm_name = "trusty"
+$vm_gui = false
+$vm_memory = 1024*2 # TODO configurable memory
+$vm_cpus = 1        # TODO configurable cpus
+
+# shared folders follow a `host:guest` format with multiples being comma 
+# separated
+#
+$shared_folders = {}
+(ENV["VAGRANT_MOUNTS"] || ".:/vagrant").split(",").each do |mount|
+  host, guest = mount.split(":")
+
+  $shared_folders[host] = guest
+end
+
+Vagrant.configure(2) do |config|
+  # base configuration
+  #
+  config.vm.define $vm_name do |c|
+    c.vm.box = $box
+
+    c.vm.provider :virtualbox do |vb|
+      vb.gui = $vm_gui
+      vb.memory = $vm_memory
+      vb.cpus = $vm_cpus
+    end
+
+    c.vm.hostname = vm_name
+
+    $shared_folders.each do |host, local|
+      # TODO pass in owner
+      c.vm.synced_folder host, local, owner: "nowk"
+    end
+  end
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine. In the example below,
+  # accessing "localhost:8080" will access port 80 on the guest machine.
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  [
+    1337, 
+    7331, 
+    3000, 
+    4000
+  ].each do |port|
+    config.vm.network "forwarded_port", guest: pott, host: port
+  end
+
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  # config.vm.network "private_network", ip: "192.168.33.10"
+
+  # Create a public network, which generally matched to bridged network.
+  # Bridged networks make the machine appear as another physical device on
+  # your network.
+  # config.vm.network "public_network"
+
+  # Define a Vagrant Push strategy for pushing to Atlas. Other push strategies
+  # such as FTP and Heroku are also available. See the documentation at
+  # https://docs.vagrantup.com/v2/push/atlas.html for more information.
+  # config.push.define "atlas" do |push|
+  #   push.app = "YOUR_ATLAS_USERNAME/YOUR_APPLICATION_NAME"
+  # end
+
+  # Enable provisioning with a shell script. Additional provisioners such as
+  # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
+  # documentation for more information about their specific syntax and use.
+  # config.vm.provision "shell", inline: <<-SHELL
+  #   sudo apt-get update
+  #   sudo apt-get install -y apache2
+  # SHELL
+  # TODO pass in user to build.sh which should create the user
+  config.vm.provision "shell", path: "build.sh"
+end
---
+++ build.sh
@@ -0,0 +1,120 @@
+#!/bin/sh
+set -e
+
+user=nowk
+password=astrongpassword
+
+if id $user > /dev/null 2>&1 ; then
+	echo "User $user already exists"
+else
+	echo "Creating user $user"
+
+	sudo adduser $user \
+		--gecos "*" \
+		--disabled-password
+
+	sudo sh -c "echo '$user ALL=(ALL:ALL) ALL' >> /etc/sudoers"
+
+	echo "$user:$password" | sudo chpasswd
+fi
+
+sudo apt-get update
+
+# need ppa for latest git (trusty needs this)
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository ppa:git-core/ppa
+
+sudo apt-get update
+sudo apt-get -y install curl git-core tmux zsh
+
+exec echo $password | sudo -S -i -u $user /bin/sh - << eof
+# oh my zsh
+# this throws an error, absorb with set +e
+set +e
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+set -e
+sh -c "echo '${password}' | chsh -s $(which zsh)"
+
+# dotfiles
+git clone https://github.com/nowk/prospect.git
+cd prospect/ \
+	&& git checkout pixel-2
+cd \
+	&& ln -s ~/prospect ~/.prospect \
+	&& ln -s ~/.prospect/git.config ~/.gitconfig \
+	&& ln -s ~/.prospect/git.ignore ~/.gitignore_global \
+	&& ln -s ~/.prospect/tmux ~/.tmux.conf \
+	&& ln -s ~/.prospect/vim ~/.vim \
+	&& ln -s ~/.prospect/vimrc ~/.vimrc \
+	&& mv ~/.zshrc ~/.zshrc.before-ln-s \
+	&& ln -s ~/.prospect/zshrc ~/.zshrc \
+	&& ln -s ~/.prospect/zshrc.local ~/.zshrc.local \
+	&& ln -s ~/.prospect/oh-my-zsh/themes/normalt.zsh-theme ~/.oh-my-zsh/themes/normalt.zsh-theme
+
+# terminfos
+cd ~/.prospect/terminfo \
+	&& tic screen-256color-italic.terminfo \
+	&& tic xterm-256color-italic.terminfo
+eof
+
+# vim
+sudo apt-get -y install vim-nox
+
+exec echo $password | sudo -S -i -u $user /bin/sh - << eof
+set -e
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# git clone https://github.com/nowk/genericdc.git ~/.vim/bundle/genericdc
+
+# supress output, it loads a vim
+# send <enter>
+echo | vim +PluginInstall +qall > /dev/null 2>&1
+eof
+
+# basics to compile stuff
+sudo apt-get -y install \
+	build-essential \
+	cmake \
+	software-properties-common \
+	python-dev \
+	python-pip \
+	libncurses-dev
+
+exec echo $password | sudo -S -i -u $user /bin/sh - << eof
+set -e
+if [ ! -e ~/opt ] ; then
+	mkdir ~/opt
+fi
+
+# tig
+cd ~/opt \
+	&& git clone https://github.com/jonas/tig.git
+cd tig \
+	&& make prefix=/usr/local
+echo '$password' | sudo -S make install prefix=/usr/local
+cd .. \
+	&& rm -rf tig
+
+# golang
+cd ~/opt \
+	&& git clone https://github.com/golang/go.git
+cd go \
+	&& git checkout go1.4.2
+cd src \
+	&& ./all.bash
+
+# load the export file to get the gopaths
+. ~$user/prospect/exports/golang
+
+# finish vim; our ycm requires go
+cd ~/.vim/bundle/YouCompleteMe \
+	&&./install.sh --clang-completer --gocode-completer
+
+# docker
+set +e
+curl -sSL https://get.docker.com/ | sh
+
+set -e
+# TODO install latest rocket
+eof
